183e4c5aa7427e777b09fbfcc3470631ede05238
Juan11211 <avilesjuan628@gmail.com>
2023-10-24 11:03:44 -0400

    done with first exercise

---
+++ simple_functions.js
@@ -0,0 +1,26 @@
+"use strict"
+
+
+function favoriteThings(){
+    console.log("Juan")
+    console.log("Transformers")
+    console.log("Music in general")
+}
+
+function whyImHere(){
+    let message = "Im here because I love coding and this was a perfect way to break into the field."
+
+    console.log(message)
+}
+
+function favoritePlace(){
+    let message = "One of my favorite places is different. I like to go on cruises that takes me to different destinations."
+
+    console.log(message)
+}
+
+favoritePlace();
+
+whyImHere();
+
+favoriteThings();
