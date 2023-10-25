64b1652b9cd10c9bfc032e9cf45de1c75845f969
Juan11211 <avilesjuan628@gmail.com>
2023-10-25 11:18:33 -0400

    Forgot to add middle name

--- Strings/parsing.js
+++ Strings/parsing.js
@@ -13,10 +13,15 @@ function parseAndDisplayName(name) {
   // Extract the last name
   let displayLast = name.substring(lastName + 1);
 
+  // middle name
+  let displayMiddleName = name.substring(firstName + 1, lastName);
+
   console.log(`First name is: ${displayFirst}`);
+  console.log(`Middle Name is: ${displayMiddleName}`)
   console.log(`Last name is: ${displayLast}`);
+
 }
 
 parseAndDisplayName("Brenda Kaye");
-parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Ian Burger Auston");
 parseAndDisplayName("Siddalee Grace");
