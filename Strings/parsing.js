f47496fd7e88cbeefd2caaed7b41d95dc06b7cc6
Juan11211 <avilesjuan628@gmail.com>
2023-10-25 11:15:00 -0400

    done with 3, working on 2

--- Strings/parsing.js
+++ Strings/parsing.js
@@ -1,19 +1,22 @@
 "use strict";
 
-let name = "Brenda Kaye";
+function parseAndDisplayName(name) {
+  // Find the index of the first name
+  let firstName = name.indexOf(" ");
 
-// Find the index of the first name
-let firstName = name.indexOf(" ");
+  // Find the index of the last name
+  let lastName = name.lastIndexOf(" ");
 
-// Find the index of the last name
-let lastName = name.lastIndexOf(" ");
+  // Extract the first name
+  let displayFirst = name.substring(0, firstName);
 
-// Extract the first name
-let displayFirst = name.substring(0, firstName);
+  // Extract the last name
+  let displayLast = name.substring(lastName + 1);
 
-// Extract the last name
-let displayLast = name.substring(lastName + 1);
+  console.log(`First name is: ${displayFirst}`);
+  console.log(`Last name is: ${displayLast}`);
+}
 
-console.log(`First name is: ${displayFirst}`);
-console.log(`Last name is: ${displayLast}`);
-
+parseAndDisplayName("Brenda Kaye");
+parseAndDisplayName("Ian Auston");
+parseAndDisplayName("Siddalee Grace");
--- Strings/part_codes.js
+++ Strings/part_codes.js
@@ -1,5 +1,34 @@
-"use strict"
+"use strict";
 
-function getSupplier(code){
-    let supplierCode = 
+function getSupplier(code) {
+    // Find the index of the colon
+    let supplierCodeIndex = code.indexOf(":");
+    
+    // Return all characters before the colon
+    return code.substring(0, supplierCodeIndex);
 }
+
+function getProductNumber(code) {
+    // Find the index of the : and -
+    let colonIndex = code.indexOf(":");
+    let hyphenIndex = code.indexOf("-");
+    
+    // Return all characters between the : and -
+    return code.substring(colonIndex + 1, hyphenIndex);
+}
+
+function getSize(code) {
+    // Find the index of the -
+    let hyphenIndex = code.indexOf("-");
+    
+    // Return all characters after the -
+    return code.substring(hyphenIndex + 1);
+}
+
+const productInfo = "DI:12345-M";
+
+const supplier = getSupplier(productInfo);
+const productNumber = getProductNumber(productInfo);
+const size = getSize(productInfo);
+
+console.log('The size ' + size + ' part ' + productNumber + ' is supplied by ' + supplier);
