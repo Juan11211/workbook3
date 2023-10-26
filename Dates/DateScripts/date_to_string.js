e9cc4fb1858d3ad506daf3651fee614dc21251ff
Juan11211 <avilesjuan628@gmail.com>
2023-10-25 23:46:04 -0400

    need help

---
+++ Dates/DateScripts/date_to_string.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let currentDate = new Date();
+
+let stringDate = new Date().toString();
+
+let dateString = new Date().toDateString();
+
+let utcString = new Date().toUTCString();
+
+let localeString = new Date().toLocaleString();
+
+console.log(stringDate);
+console.log(dateString);
+console.log(utcString);
+console.log(localeString);
+
---
+++ Dates/DateScripts/more_date_conversion.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let birthday = new Date("09/17/1995").toLocaleString();
+
+let sonsBirthday = new Date("003/21/2016 ").toLocaleString();
+
+let daughterBday = new Date("2019/09/02").toLocaleString();
+
+console.log(birthday)
+console.log(sonsBirthday)
+console.log(daughterBday)
